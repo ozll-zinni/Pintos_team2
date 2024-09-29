0095c8129990a18aac4e00b5014cc95de0bbb89a
Ubuntu <ubuntu@ip-172-31-10-69.ap-northeast-2.compute.internal>
2024-09-29 11:58:34 +0000

    hello

--- include/threads/synch.h
+++ include/threads/synch.h
@@ -10,11 +10,19 @@ struct semaphore {
 	struct list waiters;        /* List of waiting threads. */
 };
 
+/* One semaphore in a list. */
+struct semaphore_elem {
+	struct list_elem elem;              /* List element. */
+	struct semaphore semaphore;         /* This semaphore. */
+};
+
+
 void sema_init (struct semaphore *, unsigned value);
 void sema_down (struct semaphore *);
 bool sema_try_down (struct semaphore *);
 void sema_up (struct semaphore *);
 void sema_self_test (void);
+bool cmp_sem_priority(const struct list_elem *a, const struct list_elem *b, void *aux);
 
 /* Lock. */
 struct lock {
@@ -27,6 +35,9 @@ void lock_acquire (struct lock *);
 bool lock_try_acquire (struct lock *);
 void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
+
+void remove_with_lock(struct lock *lock);
+void refresh_prioirty(void);
 
 /* Condition variable. */
 struct condition {
